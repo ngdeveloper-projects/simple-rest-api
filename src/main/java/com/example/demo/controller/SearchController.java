50a925c98bf4c5fa58ffdb9a490c599f388c88a6
agnaveen <mail2agn@gmail.com>
2021-07-22 12:21:27 +0530

    base code

---
+++ src/main/java/com/example/demo/controller/SearchController.java
@@ -0,0 +1,24 @@
+package com.example.demo.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/search")
+public class SearchController {
+
+	@GetMapping("/popular")
+	public List<String> getPopularSearchList() {
+		List<String> popularSearchList = new ArrayList<>();
+		popularSearchList.add("Mobile");
+		popularSearchList.add("Laptop");
+		popularSearchList.add("Headset");
+		popularSearchList.add("Formal Shirts");
+		return popularSearchList;
+	}
+
+}
